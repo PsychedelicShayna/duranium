f703562ea595dce5b509d75434d3aa65f0bb4ceb
Shayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-09-04 04:55:06 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+const MESSAGE: &str = "
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Implementation underway; check back later.
+You can contact me by creating an issue on GitHub: PsychedelicShayna
+Repository: https://github.com/PsychedelicShayna/duranium
+-----BEGIN PGP SIGNATURE-----
+
+iQIzBAEBCAAdFiEEZquQQ54RbIDOP83v9e7Um9DduFUFAmT1fbsACgkQ9e7Um9Dd
+uFXPhA/8CIXB45B8+P4jq+bVbVq8blNGvUnVAL2t9bHsXShDOAS1mXgxYo3PQyhN
+tSDufgcGrQ/ayePhyc9nxGDHcdQKIoH5NJtNmk48CByIzGblnmfBKMg/W59vGD/4
+9AgVSoF0YZAVIrbr+d93r7V+t/0N/zOxVkiky8ddaZbKwnfiRyKCuJHda0x696cw
+MEtrpWcb4j1d6HNbzGV5nO8KgujW0NFZ+Y9yz2PqLUDriqgSWirui7gdcCjXhwkl
+Lv9qj83AXc9P1z5b2c3cSpTwNCY+Nb2Ar8QJjUWwu2VwoEhbpBH5V3WOEYdZVUSu
+shQhxeUudleT3YNIog2tXO4ys5RACqk+bHoZijbE+PyiqBCdDHkt7JR+kw6dqKK4
+kvwX2E9YLxlktZkSdiBHEtrXxnKfHPVp1CDB3JzlrsM0GrHbMh/dMw8Bo4DZzY6e
+bkGNJNqU4hgRh51OtwMzent1BfHQHtzizf1m4cm0PgKK9kP0lUhoWMJg/TKryepE
+KV7vuY+3snHRjipemFwMYPVHzPOQ5Xvg2DJlQZsepj2yquLTS++sYWYHsn+8/cBt
+x1LzupW4qQJqEn2cyYP9PbdbieL4BTGjzPcnp2W80seAU9vVphMnMgUfE3BHHxWt
+DWHrcHNwL2we5UBUxf+dsHxL+m0bTWR4t8TKgqHoTtU1uSVfgSs=
+=V/92
+-----END PGP SIGNATURE-----
+";
+
+
+fn main() {
+    println!("{}", MESSAGE);
+}
